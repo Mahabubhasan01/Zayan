fbd1e7610fef6763a558ae2a20d3c38b5aa3565c
Mahabubhasan01 <mahabubhasaan@outlook.com>
2023-05-05 12:15:24 +0600

    scroll down and some file impliment

---
+++ src/components/home/HeaderSocials.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HeaderSocials = () => {
+    return (
+        <div className='home__socials'>
+            <a href="" className="home__socials-link" target='_blank'><i class="fa-brands fa-instagram"></i></a>
+            <a href="" className="home__socials-link" target='_blank'><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100090812792251" className="home__socials-link" target='_blank'><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://github.com/Mahabubhasan01" className="home__socials-link" target='_blank'><i class="fa-brands fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/mahabub-hasaan/" className="home__socials-link" target='_blank'><i class="fa-brands fa-linkedin"></i></a>
+            
+        </div>
+    );
+};
+
+export default HeaderSocials;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import '../../styles/home.css'
+import React from "react";
+import "../../styles/home.css";
+import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from "./HeaderSocials";
+import ScrollDown from "./ScrollDown";
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Sanvi Rahman</h1>
+        <span className="home__education">I`m a Full-Stack developer</span>
+        <HeaderSocials />
+        <a href="#contact" className="btn">Hire Me</a>
+        <ScrollDown/>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
